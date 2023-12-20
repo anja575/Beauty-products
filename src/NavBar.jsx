abdbde7afca1575a49393f5f4daed5b894145a65
anja575 <anja.cirkovic00@gmail.com>
2023-12-20 23:33:49 +0100

    Created pages and navigation bar

---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <div>
+        <Link to='/'>Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/cart">Cart</Link>
+    </div >
+  )
+}
+
+export default NavBar
