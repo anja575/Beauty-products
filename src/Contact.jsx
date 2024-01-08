056a3743ecf1e685f69b022ce58a916d4d0e3e79
anja575 <anja.cirkovic00@gmail.com>
2024-01-09 00:04:11 +0100

    Stylized Contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -27,34 +27,34 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <h1>Contact us</h1>
+      <h1 className='contact-title'>Contact us</h1>
     <div class="contact-form">
       <form onSubmit={signal}>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="name">Name</label>
           <input type="text" placeholder='Name' id="name" name="name" required/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="surname">Surname</label>
           <input type="text" placeholder='Surname' id="surname" name="surname" required/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="email">Email</label>
           <input type="email" placeholder='Email' id="email" name="email" required/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="city">City</label>
           <input type="text" id="city" placeholder='City' name="city" defaultValue={informations ? informations.city : ''}/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="region">Region</label>
           <input type="text" id="region" placeholder='Region' name="region" defaultValue={informations ? informations.region : ''}/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="country">Country</label>
           <input type="text" id="country" placeholder='Country' name="country" defaultValue={informations ? informations.country_name : ''}/>
         </div>
-        <div className="form-group">
+        <div className="contact-field">
           <label htmlFor="message">Message</label>
           <textarea id="message" placeholder='Message' name="message" required></textarea>
         </div>
